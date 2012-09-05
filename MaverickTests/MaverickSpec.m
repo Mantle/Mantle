694430ce73e082f98edd56248a22c4b9ef5f05cd
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-09-04 23:55:13 -0700

    Added Specta and Expecta for unit tests

---
+++ MaverickTests/MaverickSpec.m
@@ -0,0 +1,15 @@
+//
+//  MaverickSpec.m
+//  MaverickSpec
+//
+//  Created by Justin Spahr-Summers on 2012-09-04.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+SpecBegin(Maverick)
+
+it(@"should use Specta", ^{
+	expect(YES).to.beTruthy();
+});
+
+SpecEnd
