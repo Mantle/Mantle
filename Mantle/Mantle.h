04823c211078316c80f364592f06136c0fe421bf
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-09-24 14:33:11 -0700

    Add NSArray manipulation additions to framework header

--- Mantle/Mantle.h
+++ Mantle/Mantle.h
@@ -9,6 +9,7 @@
 #import "MTLModel.h"
 #import "MTLValueTransformer.h"
 #import "NSArray+MTLHigherOrderAdditions.h"
+#import "NSArray+MTLManipulationAdditions.h"
 #import "NSDictionary+MTLHigherOrderAdditions.h"
 #import "NSOrderedSet+MTLHigherOrderAdditions.h"
 #import "NSSet+MTLHigherOrderAdditions.h"
