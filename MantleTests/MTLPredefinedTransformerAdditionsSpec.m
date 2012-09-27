d75e592d25bfada74317819962ca44551bc1f37c
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-09-27 10:45:25 -0700

    Moved URL value transformer test into a new spec

---
+++ MantleTests/MTLPredefinedTransformerAdditionsSpec.m
@@ -0,0 +1,23 @@
+//
+//  MTLPredefinedTransformerAdditionsSpec.m
+//  Mantle
+//
+//  Created by Justin Spahr-Summers on 2012-09-27.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+SpecBegin(MTLPredefinedTransformerAdditions)
+
+it(@"should define a URL value transformer", ^{
+	NSValueTransformer *transformer = [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+	expect(transformer).notTo.beNil();
+
+	NSString *URLString = @"http://www.github.com/";
+	expect([transformer transformedValue:URLString]).to.equal([NSURL URLWithString:URLString]);
+	expect([transformer reverseTransformedValue:[NSURL URLWithString:URLString]]).to.equal(URLString);
+
+	expect([transformer transformedValue:nil]).to.beNil();
+	expect([transformer reverseTransformedValue:nil]).to.beNil();
+});
+
+SpecEnd
