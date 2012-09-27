7124fca90054abd2d8f48f7ade86c9ecc826a149
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-09-27 10:49:39 -0700

    Tests for single external representation transformer

--- MantleTests/MTLPredefinedTransformerAdditionsSpec.m
+++ MantleTests/MTLPredefinedTransformerAdditionsSpec.m
@@ -6,11 +6,14 @@
 //  Copyright (c) 2012 GitHub. All rights reserved.
 //
 
+#import "MTLTestModel.h"
+
 SpecBegin(MTLPredefinedTransformerAdditions)
 
 it(@"should define a URL value transformer", ^{
 	NSValueTransformer *transformer = [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
 	expect(transformer).notTo.beNil();
+	expect([transformer.class allowsReverseTransformation]).to.beTruthy();
 
 	NSString *URLString = @"http://www.github.com/";
 	expect([transformer transformedValue:URLString]).to.equal([NSURL URLWithString:URLString]);
@@ -18,6 +21,28 @@ it(@"should define a URL value transformer", ^{
 
 	expect([transformer transformedValue:nil]).to.beNil();
 	expect([transformer reverseTransformedValue:nil]).to.beNil();
+});
+
+describe(@"external representation transformer", ^{
+	__block MTLTestModel *model;
+	__block NSValueTransformer *transformer;
+
+	before(^{
+		model = [[MTLTestModel alloc] init];
+		expect(model).notTo.beNil();
+
+		transformer = [NSValueTransformer mtl_externalRepresentationTransformerWithModelClass:model.class];
+		expect(transformer).notTo.beNil();
+	});
+
+	it(@"should transform an external representation into a model", ^{
+		expect([transformer transformedValue:model.externalRepresentation]).to.equal(model);
+	});
+
+	it(@"should transform a model into an external representation", ^{
+		expect([transformer.class allowsReverseTransformation]).to.beTruthy();
+		expect([transformer reverseTransformedValue:model]).to.equal(model.externalRepresentation);
+	});
 });
 
 SpecEnd
