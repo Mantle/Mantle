966af202f6ca9293588ac7e7d881f5a01446cc2b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-10-15 18:17:41 -0700

    Remove CGRectDivideExcludingIntersection()
    The function was impossible to use and not very helpful.

--- Mantle/CGGeometry+MTLConvenienceAdditions.h
+++ Mantle/CGGeometry+MTLConvenienceAdditions.h
@@ -65,38 +65,6 @@ CGRect CGRectSlice (CGRect rect, CGFloat amount, CGRectEdge edge);
 CGRect CGRectGrow (CGRect rect, CGFloat amount, CGRectEdge edge);
 
 /**
- * Divides a source rectangle into two component rectangles, determined by
- * "cutting out" a region that intersects with another rectangle.
- *
- * This function will do the following:
- *
- *  1. Determine the intersection of `rect` and `intersectingRect`.
- *  2. If `rect` does not intersect `intersectingRect`, set `slice` to `rect`,
- *  set `remainder` to `CGRectNull`, and return immediately.
- *  3. _Starting from_ the given edge, cut `rect` until the start of the
- *  intersection is reached. If the intersection starts outside of `rect`, set
- *  `slice` to `CGRectNull`; otherwise, set `slice` to the cut region (which may
- *  be of zero size along the cutting axis).
- *  4. Starting from the given edge, skip the entire length of the intersection.
- *  5. If the intersection ends outside of `rect`, set `remainder` to
- *  `CGRectNull`; otherwise, set `remainder` to whatever region of `rect`
- *  remains (which may be of zero size along the cutting axis).
- *
- * @param rect The rectangle to divide.
- * @param slice Upon return, the portion of `rect` starting from `edge` and
- * continuing until the intersection of `rect` and `intersectingRect`. This
- * argument may be `NULL` to not return the slice.
- * @param remainder Upon return, the portion of `rect` starting _after_ the
- * intersection of `rect` and `intersectingRect`, and continuing until the end
- * of `rect`. This argument may be `NULL` to not return the remainder.
- * @param intersectingRect A rectangle to intersect with `rect` in order to
- * determine where and how much to cut.
- * @param edge The edge from which cutting begins, with cutting proceeding
- * toward the opposite edge.
- */
-void CGRectDivideExcludingIntersection (CGRect rect, CGRect *slice, CGRect *remainder, CGRect intersectingRect, CGRectEdge edge);
-
-/**
  * Divides a source rectangle into two component rectangles, skipping the given
  * amount of padding in between them.
  *
