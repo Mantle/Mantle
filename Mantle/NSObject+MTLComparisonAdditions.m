9764210e7b941456c362345d2a0d3a35345ea70a
Josh Vera <vera@github.com>
2012-10-26 14:30:31 -0400

    Spaces to tabs

--- Mantle/NSObject+MTLComparisonAdditions.m
+++ Mantle/NSObject+MTLComparisonAdditions.m
@@ -12,5 +12,5 @@
 #import "NSObject+MTLComparisonAdditions.h"
 
 BOOL MTLEqualObjects(id obj1, id obj2) {
-    return (obj1 == obj2 || [obj1 isEqual:obj2]);
+	return (obj1 == obj2 || [obj1 isEqual:obj2]);
 }
--- MantleTests/MTLComparisonAdditionsSpec.m
+++ MantleTests/MTLComparisonAdditionsSpec.m
@@ -17,25 +17,25 @@ describe(@"MTLEqualObjects", ^{
 	id obj1 = @"Test1";
 	id obj2 = @"Test2";
 
-    it(@"returns true when given two values of nil", ^{
-        expect(MTLEqualObjects(nil, nil)).to.beTruthy();
-    });
+	it(@"returns true when given two values of nil", ^{
+		expect(MTLEqualObjects(nil, nil)).to.beTruthy();
+	});
 
-    it(@"returns true when given two equal objects", ^{
-        expect(MTLEqualObjects(obj1, obj1)).to.beTruthy();
-    });
+	it(@"returns true when given two equal objects", ^{
+		expect(MTLEqualObjects(obj1, obj1)).to.beTruthy();
+	});
 
-    it(@"returns false when given two inequal objects", ^{
-        expect(MTLEqualObjects(obj1, obj2)).to.beFalsy();
-    });
+	it(@"returns false when given two inequal objects", ^{
+		expect(MTLEqualObjects(obj1, obj2)).to.beFalsy();
+	});
 
-    it(@"returns false when given an object and nil", ^{
-        expect(MTLEqualObjects(obj1, nil)).to.beFalsy();
-    });
+	it(@"returns false when given an object and nil", ^{
+		expect(MTLEqualObjects(obj1, nil)).to.beFalsy();
+	});
 
-    it(@"returns the same value when given symmetric arguments", ^{
-        expect(MTLEqualObjects(obj2, obj1)).to.equal(MTLEqualObjects(obj1, obj2));
-    });
+	it(@"returns the same value when given symmetric arguments", ^{
+		expect(MTLEqualObjects(obj2, obj1)).to.equal(MTLEqualObjects(obj1, obj2));
+	});
 });
 
 SpecEnd
