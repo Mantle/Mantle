dc3b5d9840493fb2eca311a1e361f5d702b60fb5
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-02-13 01:30:28 -0800

    Refactor and split up MTLModel and MTLJSONAdapter tests

---
+++ MantleTests/MTLJSONAdapterSpec.m
@@ -0,0 +1,96 @@
+//
+//  MTLJSONAdapterSpec.m
+//  Mantle
+//
+//  Created by Justin Spahr-Summers on 2013-02-13.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "MTLTestModel.h"
+
+SpecBegin(MTLJSONAdapter)
+
+it(@"should initialize from JSON", ^{
+	NSDictionary *values = @{
+		@"username": NSNull.null,
+		@"count": @"5",
+	};
+
+	MTLJSONAdapter *adapter = [[MTLJSONAdapter alloc] initWithJSONDictionary:values modelClass:MTLTestModel.class];
+	expect(adapter).notTo.beNil();
+
+	MTLTestModel *model = (id)adapter.model;
+	expect(model).notTo.beNil();
+	expect(model.name).to.beNil();
+	expect(model.count).to.equal(5);
+
+	expect(adapter.JSONDictionary).to.equal(values);
+});
+
+it(@"should initialize from a model", ^{
+	MTLTestModel *model = [MTLTestModel modelWithDictionary:@{
+		@"name": @"foobar",
+		@"count": @5,
+	}];
+
+	MTLJSONAdapter *adapter = [[MTLJSONAdapter alloc] initWithModel:model];
+	expect(adapter).notTo.beNil();
+	expect(adapter.model).to.beIdenticalTo(model);
+
+	NSDictionary *JSONDictionary = @{
+		@"username": @"foobar",
+		@"count": @"5",
+	};
+
+	expect(adapter.JSONDictionary).to.equal(JSONDictionary);
+});
+
+it(@"should initialize nested key paths from JSON", ^{
+	NSDictionary *values = @{
+		@"username": @"foo",
+		@"nested": @{ @"name": @"bar" },
+		@"count": @"0"
+	};
+
+	MTLTestModel *model = [MTLJSONAdapter modelOfClass:MTLTestModel.class fromJSONDictionary:values];
+	expect(model).notTo.beNil();
+
+	expect(model.name).to.equal(@"foo");
+	expect(model.count).to.equal(0);
+	expect(model.nestedName).to.equal(@"bar");
+
+	expect([MTLJSONAdapter JSONDictionaryFromModel:model]).to.equal(values);
+});
+
+it(@"should fail to initialize with a nil JSON dictionary", ^{
+	MTLJSONAdapter *adapter = [[MTLJSONAdapter alloc] initWithJSONDictionary:nil modelClass:MTLTestModel.class];
+	expect(adapter).to.beNil();
+});
+
+it(@"should ignore unrecognized JSON keys", ^{
+	NSDictionary *values = @{
+		@"foobar": @"foo",
+		@"count": @"2",
+		@"_": NSNull.null,
+		@"username": @"buzz",
+		@"nested": @{ @"name": @"bar", @"stuffToIgnore": @5, @"moreNonsense": NSNull.null },
+	};
+
+	MTLTestModel *model = [MTLJSONAdapter modelOfClass:MTLTestModel.class fromJSONDictionary:values];
+	expect(model).notTo.beNil();
+
+	expect(model.name).to.equal(@"buzz");
+	expect(model.count).to.equal(2);
+	expect(model.nestedName).to.equal(@"bar");
+});
+
+it(@"should fail to initialize if JSON dictionary validation fails", ^{
+	NSDictionary *values = @{
+		@"username": @"this is too long a name",
+	};
+
+	MTLTestModel *model = [MTLJSONAdapter modelOfClass:MTLTestModel.class fromJSONDictionary:values];
+	expect(model).to.beNil();
+});
+
+SpecEnd
