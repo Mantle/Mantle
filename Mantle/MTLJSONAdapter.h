372c928d6e4c5835474ac018f9f747a55f4fd992
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-02-12 20:38:47 -0800

    Documentation for MTLJSONAdapter

--- Mantle/MTLJSONAdapter.h
+++ Mantle/MTLJSONAdapter.h
@@ -8,19 +8,58 @@
 
 #import <Foundation/Foundation.h>
 
+// A MTLModel object that supports being parsed from and serialized to JSON.
 @protocol MTLJSONSerializing
 @optional
 
+// Specifies how to map property keys to different key paths in JSON.
+//
+// Subclasses overriding this method should combine their values with those of
+// `super`.
+//
+// Any property keys not present in the dictionary are assumed to match the JSON
+// key that should be used. Any keys associated with NSNull will not participate
+// in JSON serialization.
+//
+// Returns a dictionary mapping property keys to JSON key paths (as strings) or
+// NSNull values.
 + (NSDictionary *)JSONKeyPathsByPropertyKey;
 
+// Specifies how to convert a JSON value to the given property key. If
+// reversible, the transformer will also be used to convert the property value
+// back to JSON.
+//
+// If the receiver implements a `+<key>JSONTransformer` method, MTLJSONAdapter
+// will use the result of that method instead.
+//
+// Returns a value transformer, or nil if no transformation should be performed.
 + (NSValueTransformer *)JSONTransformerForKey:(NSString *)key;
-+ (NSDictionary *)migrateJSONRepresentation:(NSDictionary *)externalRepresentation fromVersion:(NSUInteger)fromVersion;
 
 @end
 
+// Converts a MTLModel object to and from a JSON dictionary.
 @interface MTLJSONAdapter : NSObject
 
-+ (NSDictionary *)JSONDictionaryFromModel:(MTLModel<MTLJSONSerializing> *)model;
-+ (id)modelOfClass:(Class)modelClass fromJSONDictionary:(NSDictionary *)dict;
+// The model object that the receiver was initialized with, or that the receiver
+// parsed from a JSON dictionary.
+@property (nonatomic, strong, readonly) MTLModel<MTLJSONSerializing> *model;
+
+// Initializes the receiver by attempting to parse a JSON dictionary into
+// a model object.
+//
+// JSONDictionary - A dictionary representing JSON data. This should match the
+//                  format returned by NSJSONSerialization.
+// modelClass     - The MTLModel subclass to attempt to parse from the JSON.
+//                  This class must conform to <MTLJSONSerializing>.
+//
+// Returns an initialized adapter upon success, or nil if a parsing error
+// occurred.
+- (id)initWithJSONDictionary:(NSDictionary *)JSONDictionary modelClass:(Class)modelClass;
+
+// Initializes the receiver with an existing model.
+- (id)initWithModel:(MTLModel<MTLJSONSerializing> *)model;
+
+// Serializes the receiver's `model` into a JSON dictionary.
+- (NSDictionary *)JSONDictionary;
 
 @end
