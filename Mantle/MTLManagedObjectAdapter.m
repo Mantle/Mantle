0d0faa19801f1cef9e4245d1dbf900928ce66d06
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-04-01 13:34:54 -0700

    Move MTLManagedObjectAdapter into the main project
    As long as we don't ever reference Core Data classes directly, we should be able to use methods from them.

--- Mantle/MTLManagedObjectAdapter.m
+++ Mantle/MTLManagedObjectAdapter.m
@@ -7,14 +7,21 @@
 //
 
 #import "MTLManagedObjectAdapter.h"
+#import "MTLModel.h"
 
 @implementation MTLManagedObjectAdapter
 
 + (id)modelOfClass:(Class)modelClass fromManagedObject:(NSManagedObject *)managedObject error:(NSError **)error {
+	Class managedObjectClass = NSClassFromString(@"NSManagedObject");
+	NSAssert(managedObjectClass != nil, @"CoreData.framework must be linked to use MTLManagedObjectAdapter");
+
 	return nil;
 }
 
 + (NSManagedObject *)managedObjectFromModel:(MTLModel<MTLManagedObjectSerializing> *)model insertingIntoContext:(NSManagedObjectContext *)context error:(NSError **)error {
+	Class managedObjectClass = NSClassFromString(@"NSManagedObject");
+	NSAssert(managedObjectClass != nil, @"CoreData.framework must be linked to use MTLManagedObjectAdapter");
+
 	return nil;
 }
 
