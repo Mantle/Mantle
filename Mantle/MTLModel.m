c54a399944a7a3ed6bf624751d507196ebbafa52
Robert Böhnke <robb@robb.is>
2013-07-17 11:20:40 +0200

    Extracting MTLValidateAndSetValue

--- Mantle/MTLModel.m
+++ Mantle/MTLModel.m
@@ -21,6 +21,50 @@
 // Used to cache the reflection performed in +propertyKeys.
 static void *MTLModelCachedPropertyKeysKey = &MTLModelCachedPropertyKeysKey;
 
+// Validates a value for an object and sets it if necessary.
+//
+// obj         - The object for which the value is being validated. This value
+//               must not be nil.
+// key         - The name of one of `obj`s properties. This value must not be
+//               nil.
+// value       - The new value for the property identified by `key`.
+// forceUpdate - If set to `YES`, the value is being updated even if validating
+//               it did not change it.
+// error       - If not NULL, this may be set to any error that occurs during
+//               validation
+//
+// Returns YES if `value` could be validated and set, or NO if an error
+// occurred.
+static BOOL MTLValidateAndSetValue(id obj, NSString *key, id value, BOOL forceUpdate, NSError **error) {
+	// Mark this as being autoreleased, because validateValue may return
+	// a new object to be stored in this variable (and we don't want ARC to
+	// double-free or leak the old or new values).
+	__autoreleasing id validatedValue = value;
+
+	@try {
+		if (![obj validateValue:&validatedValue forKey:key error:error]) return NO;
+
+		if (forceUpdate || value != validatedValue) {
+			[obj setValue:validatedValue forKey:key];
+		}
+
+		return YES;
+	} @catch (NSException *ex) {
+		NSLog(@"*** Caught exception setting key \"%@\" : %@", key, ex);
+
+		// Fail fast in Debug builds.
+		#if DEBUG
+		@throw ex;
+		#else
+		if (error != NULL) {
+			*error = [NSError mtl_modelErrorWithException:ex];
+		}
+
+		return NO;
+		#endif
+	}
+}
+
 @interface MTLModel ()
 
 // Enumerates all properties of the receiver's class hierarchy, starting at the
@@ -57,24 +101,9 @@ static void *MTLModelCachedPropertyKeysKey = &MTLModelCachedPropertyKeysKey;
 	
 		if ([value isEqual:NSNull.null]) value = nil;
 
-		@try {
-			if (![self validateValue:&value forKey:key error:error]) return nil;
+		BOOL success = MTLValidateAndSetValue(self, key, value, YES, error);
 
-			[self setValue:value forKey:key];
-		} @catch (NSException *ex) {
-			NSLog(@"*** Caught exception setting key \"%@\" from %@: %@", key, dictionary, ex);
-
-			// Fail fast in Debug builds.
-			#if DEBUG
-			@throw ex;
-			#else
-			if (error != NULL) {
-				*error = [NSError mtl_modelErrorWithException:ex];
-			}
-
-			return nil;
-			#endif
-		}
+		if (!success) return nil;
 	}
 
 	return self;
@@ -167,31 +196,9 @@ static void *MTLModelCachedPropertyKeysKey = &MTLModelCachedPropertyKeysKey;
 	for (NSString *key in self.class.propertyKeys) {
 		id value = [self valueForKey:key];
 
-		// Mark this as being autoreleased, because validateValue may return
-		// a new object to be stored in this variable (and we don't want ARC to
-		// double-free or leak the old or new values).
-		__autoreleasing id validatedValue = value;
+		BOOL success = MTLValidateAndSetValue(self, key, value, NO, error);
 
-		@try {
-			if (![self validateValue:&validatedValue forKey:key error:error]) return NO;
-
-			if (value != validatedValue) {
-				[self setValue:validatedValue forKey:key];
-			}
-		} @catch (NSException *ex) {
-			NSLog(@"*** Caught exception setting key \"%@\" : %@", key, ex);
-
-			// Fail fast in Debug builds.
-			#if DEBUG
-			@throw ex;
-			#else
-			if (error != NULL) {
-				*error = [NSError mtl_modelErrorWithException:ex];
-			}
-
-			return NO;
-			#endif
-		}
+		if (!success) return NO;
 	}
 
 	return YES;
