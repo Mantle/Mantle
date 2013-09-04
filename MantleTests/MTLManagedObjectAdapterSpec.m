d2094446dfaa31ac89e9a41e49d9bae169602d81
Robert Böhnke <robb@robb.is>
2013-09-04 15:00:19 +0200

    Test uniqueness constraint specifically

--- MantleTests/MTLManagedObjectAdapterSpec.m
+++ MantleTests/MTLManagedObjectAdapterSpec.m
@@ -203,134 +203,19 @@ describe(@"with a confined context", ^{
 			expect([context save:&error]).to.beTruthy();
 			expect(error).to.beNil();
 		});
-	});
-	
-	describe(@"+propertyKeysForManagedObjectUniquing", ^{
-		__block MTLParentTestModel *parentModel;
-		__block MTLParent *parent;
 
-		NSDate *date = [NSDate dateWithTimeIntervalSince1970:12345];
-		NSString *numberString = @"12345";
-		NSString *requiredString = @"foobar";
+		it(@"should respect the uniqueness constraint", ^{
+			NSError *errorOne;
+			MTLParent *parentOne = (MTLParent *)[MTLManagedObjectAdapter managedObjectFromModel:parentModel insertingIntoContext:context error:&errorOne];
+			expect(parentOne).notTo.beNil();
+			expect(errorOne).to.beNil();
 
-		beforeEach(^{
-			// Save our managed objects
-			parent = [MTLParent insertInManagedObjectContext:context];
-			expect(parent).notTo.beNil();
+			NSError *errorTwo;
+			MTLParent *parentTwo = (MTLParent *)[MTLManagedObjectAdapter managedObjectFromModel:parentModel insertingIntoContext:context error:&errorTwo];
+			expect(parentTwo).notTo.beNil();
+			expect(errorTwo).to.beNil();
 
-			for (NSUInteger i = 0; i < 3; i++) {
-				MTLChild *child = [MTLChild insertInManagedObjectContext:context];
-				expect(child).notTo.beNil();
-
-				child.id = @(i);
-				[parent addOrderedChildrenObject:child];
-			}
-
-			for (NSUInteger i = 3; i < 6; i++) {
-				MTLChild *child = [MTLChild insertInManagedObjectContext:context];
-				expect(child).notTo.beNil();
-
-				child.id = @(i);
-				[parent addUnorderedChildrenObject:child];
-			}
-
-			parent.string = requiredString;
-
-			__block NSError *error = nil;
-			expect([context save:&error]).to.beTruthy();
-			expect(error).to.beNil();
-
-			// Make sure that pending changes are picked up too.
-			parent.number = @(numberString.integerValue);
-			parent.date = date;
-			
-			// Create our models that will be converted to managed objects.
-			parentModel = [MTLParentTestModel modelWithDictionary:@{
-				@"date": date,
-				@"numberString": @"12345",
-				@"requiredString": @"foobar"
-			} error:NULL];
-			expect(parentModel).notTo.beNil();
-
-			NSMutableArray *orderedChildren = [NSMutableArray array];
-			NSMutableSet *unorderedChildren = [NSMutableSet set];
-
-			for (NSUInteger i = 0; i < 3; i++) {
-				MTLChildTestModel *child = [MTLChildTestModel modelWithDictionary:@{
-					@"childID": @(i),
-					@"parent2": parentModel
-				} error:NULL];
-				expect(child).notTo.beNil();
-
-				[orderedChildren addObject:child];
-			}
-
-			for (NSUInteger i = 3; i < 6; i++) {
-				MTLChildTestModel *child = [MTLChildTestModel modelWithDictionary:@{
-					@"childID": @(i),
-					@"parent1": parentModel
-				} error:NULL];
-				expect(child).notTo.beNil();
-
-				[unorderedChildren addObject:child];
-			}
-
-			parentModel.orderedChildren = orderedChildren;
-			parentModel.unorderedChildren = unorderedChildren;
-		});
-
-		it(@"should fetch a unique parent managed object using numberString to maintain uniqueness", ^{
-			__block NSError *error = nil;
-			MTLParent *newParent = (MTLParent *)[MTLManagedObjectAdapter managedObjectFromModel:parentModel insertingIntoContext:context error:&error];
-			expect(newParent).notTo.beNil();
-			expect(error).to.beNil();
-			
-			expect(newParent.entity).to.equal(parentEntity);
-			expect(context.insertedObjects).notTo.contain(newParent);
-			
-			expect([newParent objectID]).to.equal([parent objectID]);
-
-			expect(newParent.date).to.equal(parent.date);
-			expect(newParent.number).to.equal(parent.number);
-			expect(newParent.string).to.equal(parent.string);
-
-			expect(parent.orderedChildren).to.haveCountOf(3);
-			expect(newParent.orderedChildren).to.haveCountOf(3);
-
-			expect(parent.unorderedChildren).to.haveCountOf(3);
-			expect(newParent.unorderedChildren).to.haveCountOf(3);
-			
-			for (NSUInteger i = 0; i < 3; i++) {
-				MTLChild *child = parent.orderedChildren[i];
-				MTLChild *newChild = parent.orderedChildren[i];
-
-				expect(newChild.entity).to.equal(childEntity);
-				expect(context.insertedObjects).notTo.contain(newChild);
-				
-				expect(newChild.id).to.equal(i);
-				expect(newChild.parent1).to.beNil();
-				expect(newChild.parent2).to.equal(newParent);
-				
-				expect([newChild objectID]).to.equal([child objectID]);
-				expect(newChild.id).to.equal(newChild.id);
-			}
-			
-			for (MTLChild *child in parent.unorderedChildren) {
-				expect(child.entity).to.equal(childEntity);
-				expect(context.insertedObjects).notTo.contain(child);
-				
-				NSUInteger childID = [[child valueForKey:@"id"] unsignedIntegerValue];
-				expect(childID).to.beGreaterThanOrEqualTo(3);
-				expect(childID).to.beLessThan(6);
-				
-				expect(child.parent1).to.equal(newParent);
-				expect(child.parent2).to.beNil();
-				
-				expect([newParent.unorderedChildren containsObject:child]).to.beTruthy();
-			}
-			
-			expect([context save:&error]).to.beTruthy();
-			expect(error).to.beNil();
+			expect([parentOne objectID]).to.equal([parentTwo objectID]);
 		});
 	});
 });
