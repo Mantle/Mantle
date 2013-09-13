084eb8742e1a94286bd8a4be1c4f3f9c48ee376d
Ryan Maxwell <ryan@cactuslab.com>
2013-09-13 16:31:35 +1200

    move `+ (NSDictionary *)JSONKeyPathsByPropertyKey;` from a required to an optional delegate method.

--- Mantle/MTLJSONAdapter.h
+++ Mantle/MTLJSONAdapter.h
@@ -12,7 +12,7 @@
 
 // A MTLModel object that supports being parsed from and serialized to JSON.
 @protocol MTLJSONSerializing
-@required
+@optional
 
 // Specifies how to map property keys to different key paths in JSON.
 //
@@ -26,8 +26,6 @@
 // Returns a dictionary mapping property keys to JSON key paths (as strings) or
 // NSNull values.
 + (NSDictionary *)JSONKeyPathsByPropertyKey;
-
-@optional
 
 // Specifies how to convert a JSON value to the given property key. If
 // reversible, the transformer will also be used to convert the property value
--- Mantle/MTLJSONAdapter.m
+++ Mantle/MTLJSONAdapter.m
@@ -96,7 +96,8 @@ static NSString * const MTLJSONAdapterThrownExceptionErrorKey = @"MTLJSONAdapter
 	if (self == nil) return nil;
 
 	_modelClass = modelClass;
-	_JSONKeyPathsByPropertyKey = [[modelClass JSONKeyPathsByPropertyKey] copy];
+	
+	_JSONKeyPathsByPropertyKey = [modelClass respondsToSelector:@selector(JSONKeyPathsByPropertyKey)] ? [[modelClass JSONKeyPathsByPropertyKey] copy] : @{};
 
 	NSMutableDictionary *dictionaryValue = [[NSMutableDictionary alloc] initWithCapacity:JSONDictionary.count];
 
@@ -153,7 +154,7 @@ static NSString * const MTLJSONAdapterThrownExceptionErrorKey = @"MTLJSONAdapter
 
 	_model = model;
 	_modelClass = model.class;
-	_JSONKeyPathsByPropertyKey = [[model.class JSONKeyPathsByPropertyKey] copy];
+	_JSONKeyPathsByPropertyKey = [model.class respondsToSelector:@selector(JSONKeyPathsByPropertyKey)] ? [[model.class JSONKeyPathsByPropertyKey] copy] : @{};
 
 	return self;
 }
