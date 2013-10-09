c6ee880f37fe0afe0cd57508c3a660405a924504
Robert Böhnke <robb@robb.is>
2013-10-09 22:12:55 +0200

    Add error handling tests for built-in transformers

---
+++ MantleTests/MTLTransformerErrorExamples.m
@@ -0,0 +1,55 @@
+//
+//  MTLTransformerErrorExamples.m
+//  Mantle
+//
+//  Created by Robert Böhnke on 10/9/13.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "MTLTransformerErrorExamples.h"
+
+#import "MTLTransformerErrorHandling.h"
+
+NSString * const MTLTransformerErrorExamples = @"MTLTransformerErrorExamples";
+
+NSString * const MTLTransformerErrorExamplesTransformer = @"MTLTransformerErrorExamplesTransformer";
+NSString * const MTLTransformerErrorExamplesInvalidTransformationInput = @"MTLTransformerErrorExamplesInvalidTransformationInput";
+NSString * const MTLTransformerErrorExamplesInvalidReverseTransformationInput = @"MTLTransformerErrorExamplesInvalidReverseTransformationInput";
+
+SharedExampleGroupsBegin(MTLTransformerErrorExamples);
+
+sharedExamplesFor(MTLTransformerErrorExamples, ^(NSDictionary *data) {
+	__block NSValueTransformer<MTLTransformerErrorHandling> *transformer;
+	__block id invalidTransformationInput;
+	__block id invalidReverseTransformationInput;
+
+	beforeEach(^{
+		transformer = data[MTLTransformerErrorExamplesTransformer];
+		invalidTransformationInput = data[MTLTransformerErrorExamplesInvalidTransformationInput];
+		invalidReverseTransformationInput = data[MTLTransformerErrorExamplesInvalidReverseTransformationInput];
+
+		expect([transformer conformsToProtocol:@protocol(MTLTransformerErrorHandling)]).to.beTruthy();
+	});
+
+	it(@"should return errors occurring during transformation", ^{
+		__block NSError *error;
+		__block BOOL success;
+
+		expect([(id)transformer transformedValue:invalidTransformationInput success:&success error:&error]).to.beNil();
+		expect(success).to.beFalsy();
+		expect(error).notTo.beNil();
+	});
+
+	it(@"should return errors occurring during reverse transformation, if the transformer supports it", ^{
+		if ([[transformer class] allowsReverseTransformation]) {
+			__block NSError *error;
+			__block BOOL success;
+
+			expect([(id)transformer reverseTransformedValue:invalidReverseTransformationInput success:&success error:&error]).to.beNil();
+			expect(success).to.beFalsy();
+			expect(error).notTo.beNil();
+		}
+	});
+});
+
+SharedExampleGroupsEnd
