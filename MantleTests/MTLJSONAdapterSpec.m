8fc086fdc56f157095e036306539d00aa8a01fa3
Robert Böhnke <robb@robb.is>
2014-05-14 01:20:24 +0200

    Don't test for exceptions

--- MantleTests/MTLJSONAdapterSpec.m
+++ MantleTests/MTLJSONAdapterSpec.m
@@ -86,20 +86,6 @@ it(@"it should initialize properties with multiple key paths from JSON", ^{
 	expect(serializationError).to.beNil();
 });
 
-it(@"should throw an exception if initialized with an illegal JSON mapping", ^{
-	expect(^{
-		return [[MTLJSONAdapter alloc] initWithModelClass:MTLIllegalJSONMappingModel1.class];
-	}).to.raise(NSInternalInconsistencyException);
-
-	expect(^{
-		return [[MTLJSONAdapter alloc] initWithModelClass:MTLIllegalJSONMappingModel2.class];
-	}).to.raise(NSInternalInconsistencyException);
-
-	expect(^{
-		return [[MTLJSONAdapter alloc] initWithModelClass:MTLIllegalJSONMappingModel3.class];
-	}).to.raise(NSInternalInconsistencyException);
-});
-
 it(@"should return nil and error with an invalid key path from JSON",^{
 	NSDictionary *values = @{
 		@"username": @"foo",
