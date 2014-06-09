68d537ab2ceb0bc4d979efebec494223eef0189d
Robert Böhnke <robb@robb.is>
2014-06-09 12:19:57 -0700

    Document validation

--- Mantle/MTLJSONAdapter.h
+++ Mantle/MTLJSONAdapter.h
@@ -160,9 +160,13 @@ extern const NSInteger MTLJSONAdapterErrorInvalidJSONMapping;
 //                  format returned by NSJSONSerialization. This argument must
 //                  not be nil.
 // error          - If not NULL, this may be set to an error that occurs during
-//                  deserializing.
+//                  deserializing or validation.
 //
-// Returns a model object, or nil if a deserialization error occurred.
+// The adapter will call -validate: on the model and consider it an error if the
+// validation fails.
+//
+// Returns a model object, or nil if a deserialization error occurred or the
+// model did not validate successfully.
 - (id)modelFromJSONDictionary:(NSDictionary *)JSONDictionary error:(NSError **)error;
 
 // Serializes a model into JSON.
