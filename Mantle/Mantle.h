37ec1d98fdd6572f00d488fd189429bd7d5815b7
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-21 11:27:24 -0700

    Recreate iOS target as a framework, misc settings

--- Mantle/Mantle.h
+++ Mantle/Mantle.h
@@ -2,17 +2,18 @@
 //  Mantle.h
 //  Mantle
 //
-//  Created by Justin Spahr-Summers on 2012-09-04.
-//  Copyright (c) 2012 GitHub. All rights reserved.
+//  Created by Justin Spahr-Summers on 2014-10-21.
+//  Copyright (c) 2014 GitHub. All rights reserved.
 //
 
-#import <Mantle/MTLJSONAdapter.h>
-#import <Mantle/MTLManagedObjectAdapter.h>
-#import <Mantle/MTLModel.h>
-#import <Mantle/MTLModel+NSCoding.h>
-#import <Mantle/MTLValueTransformer.h>
-#import <Mantle/NSArray+MTLManipulationAdditions.h>
-#import <Mantle/NSDictionary+MTLManipulationAdditions.h>
-#import <Mantle/NSObject+MTLComparisonAdditions.h>
-#import <Mantle/NSValueTransformer+MTLInversionAdditions.h>
-#import <Mantle/NSValueTransformer+MTLPredefinedTransformerAdditions.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mantle.
+FOUNDATION_EXPORT double MantleVersionNumber;
+
+//! Project version string for Mantle.
+FOUNDATION_EXPORT const unsigned char MantleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mantle/PublicHeader.h>
+
+
