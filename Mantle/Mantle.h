1348cd3dc237c9dec466832ea092d68dcf855aa4
Anton Savelev <savelev@worki.ru>
2020-09-25 16:52:31 +0300

    Remove headers

--- Mantle/Mantle.h
+++ Mantle/Mantle.h
@@ -16,20 +16,3 @@ FOUNDATION_EXPORT const unsigned char MantleVersionString[];
 
 // These imports should not be visible to SPM & SPM doesn't seem to have the target "exclude" apply to headers
 // & publicHeadersPath is a single string, so can't just list all the headers we want unless we move them into a folder
-#if MANTLE_SPM
-    // No headers
-#else
-
-#import <Mantle/MTLJSONAdapter.h>
-#import <Mantle/MTLModel.h>
-#import <Mantle/MTLModel+NSCoding.h>
-#import <Mantle/MTLValueTransformer.h>
-#import <Mantle/MTLTransformerErrorHandling.h>
-#import <Mantle/NSArray+MTLManipulationAdditions.h>
-#import <Mantle/NSDictionary+MTLManipulationAdditions.h>
-#import <Mantle/NSDictionary+MTLMappingAdditions.h>
-#import <Mantle/NSObject+MTLComparisonAdditions.h>
-#import <Mantle/NSValueTransformer+MTLInversionAdditions.h>
-#import <Mantle/NSValueTransformer+MTLPredefinedTransformerAdditions.h>
-
-#endif /* MANTLE_SPM */
