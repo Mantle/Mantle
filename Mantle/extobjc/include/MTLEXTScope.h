0a0b971648c981072b5f01b293057a89efb41f55
Petr Mifek <petr@edmstudio.com>
2021-08-29 15:24:33 +0200

    Framework header include syntax

--- Mantle/extobjc/include/MTLEXTScope.h
+++ Mantle/extobjc/include/MTLEXTScope.h
@@ -7,7 +7,7 @@
 //  Released under the MIT license.
 //
 
-#import "MTLMetamacros.h"
+#import <MTLMetamacros.h>
 
 /**
  * \@onExit defines some code to be executed when the current scope exits. The
